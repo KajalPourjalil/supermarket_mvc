cf0af01619fbe08c00dc31e16ad0930345d055c6
KajalPourjalil <rina.skyler@yahoo.com>
2024-02-19 12:16:30 +0300

    creating the support of MVC framework

--- Supermarket-mvc/Program.cs
+++ Supermarket-mvc/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllersWithViews();//injects all the services required by the Map controller method
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
+
+//all this code is the support for the MVC framework
