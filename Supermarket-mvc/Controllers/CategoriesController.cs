83a70d1f1398562472b4a49f114bab6f040fa1f0
KajalPourjalil <rina.skyler@yahoo.com>
2024-02-20 20:58:41 +0300

    first model created

--- Supermarket-mvc/Controllers/CategoriesController.cs
+++ Supermarket-mvc/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using supermarket_mvc.Models;
 
 namespace supermarket_mvc.Controllers
 {
@@ -11,16 +12,9 @@ namespace supermarket_mvc.Controllers
 
         public IActionResult Edit(int? id)
         {
-            if (id.HasValue)
-            {
-                return new ContentResult { Content = id.ToString() };
-            }
-
-            else
-            {
-                return new ContentResult { Content = "null content" };
-            };
-
+            var category = new Category {CategoryId = id.HasValue?id.Value:0};
+            
+            return View(category);
         }
     }
 }
--- Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
+++ Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625f1ef79887621743c4ec9ae9fe48ce49d2d153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6238cf1877784ceb8767db43ac79604ae58d7c1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
