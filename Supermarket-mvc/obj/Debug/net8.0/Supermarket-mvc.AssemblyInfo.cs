4feaf6b995f07d0b463f678437900369c8b01cb8
KajalPourjalil <rina.skyler@yahoo.com>
2024-03-06 15:53:38 +0300

    updates

--- Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
+++ Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243a3e1dfc1d5e6bd9337ff0522e420317e771eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50f19e6d730b32bb9fadb04ee06a60f47a637c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
