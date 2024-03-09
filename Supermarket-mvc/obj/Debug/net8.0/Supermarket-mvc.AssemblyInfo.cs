aa8e1df7c4fff696f7016575003e61ad33aef7a7
KajalPourjalil <rina.skyler@yahoo.com>
2024-03-09 21:19:07 +0300

    created sell, products, transactions and privacy in navbar

--- Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
+++ Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1948a7094e31d4a8cf8af6a70c206e52cd856a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be98df2053ce024f194acf1e0852bbda30e26c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
