c4ac8de42d736b43bdb30aefb7ada3d6a9107360
KajalPourjalil <rina.skyler@yahoo.com>
2024-03-09 18:43:38 +0300

    used HttpPost

--- Supermarket-mvc/Controllers/CategoriesController.cs
+++ Supermarket-mvc/Controllers/CategoriesController.cs
@@ -13,10 +13,18 @@ namespace supermarket_mvc.Controllers
 
         public IActionResult Edit(int? id)
         {
-            
-            var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
+            var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
 
             return View(category);
         }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
--- Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
+++ Supermarket-mvc/obj/Debug/net8.0/Supermarket-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19f9c75356cabd6d454562bf043f448b1c88f8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de8c6c6370d5b39cd69a04108e1177bb9e730ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
